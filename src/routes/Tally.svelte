cb87ead0644d4ec1e6280ed0bde44c22c8710578
BuildTools <unconfigured@null.spigotmc.org>
2023-08-17 19:23:23 -0400

    Added Simple Tally Input

---
+++ src/routes/Tally.svelte
@@ -0,0 +1,32 @@
+<script>
+    /**
+	 * @type {String}
+	 */
+    export let label;
+    let count = 0
+
+    function increment() {
+        count += 1;
+    }
+
+    function decrement() {
+        count -= 1;
+    }
+</script>
+
+<label for={label}>{label}:</label>
+<br>
+<button on:click={decrement}>-</button>
+<input bind:value = {count}
+    type="number" 
+    inputmode="numeric"
+    pattern="[0-9]+"
+    />
+<button on:click={increment}>+</button>
+<br>
+
+<style>
+    input::-webkit-inner-spin-button {
+    display: none;
+    }
+</style>
