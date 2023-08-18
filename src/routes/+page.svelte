5d498fab5e4e307427318d5f2ac7b2101950a1bc
AdityaRayapudi <arayapudi2006@gmail.com>
2023-08-18 13:52:14 -0400

    Postgre Integration

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,10 +7,18 @@
 	import Radioboxs from "../components/Radioboxs.svelte";
 </script>
 
+<form action="">
+    <ShortText label = "Name:" name = "name"/>
+    <TeamSelection/>
+    <Tally label = "Match Number" name = "match"/>
+    <Dropdown label = "Food" name = "food" 
+        selections = {{0 : "Egg", 
+                    1 : "Cheese"}}/>
+    <Toggle label = "Do you like egg?" name = "eggPrefrence"/>
+    <Radioboxs label = "What food you want?" name = "foodtwo" 
+        selections = {{0 : "Egg", 
+                    1 : "Cheese", 
+                    2 : "Popcorn"}}/>
 
-<ShortText label = {"Name:"} name = {"name"}/>
-<TeamSelection/>
-<Tally label = {"Match Number"} name = {"match"}/>
-<Dropdown label = {"Food"} selections = {{0 : "Egg", 1 : "Cheese"}} name = {"food"}/>
-<Toggle label = "Do you like egg?" name = "eggPrefrence"/>
-<Radioboxs label = "What food you want?" name = "foodtwo" selections = {{0 : "Egg", 1 : "Cheese", 2 : "Popcorn"}}/>
+    <button type="submit">Sumbit</button>
+</form>
